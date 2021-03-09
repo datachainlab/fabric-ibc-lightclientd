562c10dd5e7f6bd0604e42be5c3547aa12a01ff8
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2021-03-09 18:15:55 +0900

    implement core logic of Fabric-IBC light client daemon

--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/datachainlab/fabric-ibc-lightclientd
 go 1.16
 
 require (
+	github.com/confio/ics23 v0.6.3 // indirect
+	github.com/confio/ics23/go v0.6.3
 	github.com/cosmos/cosmos-sdk v0.40.0-rc3
 	github.com/datachainlab/fabric-ibc v0.0.0-20210118090849-c2eaee7a3314
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/tendermint/tm-db v0.6.2
+	github.com/gogo/protobuf v1.3.1
+	google.golang.org/grpc v1.33.2
 )
 
 replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.2-alpha.regen.4
---
+++ lightclient.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	ics23 "github.com/confio/ics23/go"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/store/mem"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	clienttypes "github.com/cosmos/cosmos-sdk/x/ibc/core/02-client/types"
+	connectiontypes "github.com/cosmos/cosmos-sdk/x/ibc/core/03-connection/types"
+	channeltypes "github.com/cosmos/cosmos-sdk/x/ibc/core/04-channel/types"
+	commitmenttypes "github.com/cosmos/cosmos-sdk/x/ibc/core/23-commitment/types"
+	host "github.com/cosmos/cosmos-sdk/x/ibc/core/24-host"
+	fabrictypes "github.com/datachainlab/fabric-ibc/x/ibc/light-clients/xx-fabric/types"
+)
+
+type Lightclient struct {
+	ctx   sdk.Context
+	cdc   codec.BinaryMarshaler
+	store sdk.KVStore
+	cs    *fabrictypes.ClientState
+}
+
+func NewLightclient(clientState *fabrictypes.ClientState, consensusState *fabrictypes.ConsensusState) *Lightclient {
+	// create dummy context
+	ctx := sdk.Context{}
+
+	// create codec
+	cdc := codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
+
+	// create store
+	store := mem.NewStore()
+
+	// save consensus state in store
+	if bz, err := clienttypes.MarshalConsensusState(cdc, consensusState); err != nil {
+		panic(err)
+	} else {
+		height := clientState.GetLatestHeight()
+		store.Set(host.KeyConsensusState(height), bz)
+	}
+
+	// create lightclient core
+	return &Lightclient{
+		ctx:   ctx,
+		store: store,
+		cdc:   cdc,
+		cs:    clientState,
+	}
+}
+
+func (lc *Lightclient) ClientType() string {
+	return lc.cs.ClientType()
+}
+
+func (lc *Lightclient) GetLatestHeight() clienttypes.Height {
+	return lc.cs.GetLatestHeight().(clienttypes.Height)
+}
+
+func (lc *Lightclient) IsFrozen() bool {
+	return lc.cs.IsFrozen()
+}
+
+func (lc *Lightclient) GetFrozenHeight() clienttypes.Height {
+	return lc.cs.GetFrozenHeight().(clienttypes.Height)
+}
+
+func (lc *Lightclient) Validate() error {
+	return lc.cs.Validate()
+}
+
+func (lc *Lightclient) GetProofSpecs() []*ics23.ProofSpec {
+	return lc.cs.GetProofSpecs()
+}
+
+func (lc *Lightclient) CheckHeaderAndUpdateState(header *fabrictypes.Header) (*fabrictypes.ClientState, *fabrictypes.ConsensusState, error) {
+	clientState, consensusState, err := lc.cs.CheckHeaderAndUpdateState(lc.ctx, lc.cdc, lc.store, header)
+	return clientState.(*fabrictypes.ClientState), consensusState.(*fabrictypes.ConsensusState), err
+}
+
+// TODO: define fabrictypes.Misbehaviour
+/*
+func (lc *Lightclient) CheckMisbehaviourAndUpdateState(misbehaviour *fabrictypes.Misbehaviour) (*fabrictypes.ClientState, error) {
+	clientState, err := lc.cs.CheckMisbehaviourAndUpdateState(lc.ctx, lc.cdc, lc.store, misbehaviour)
+	return clientState.(*fabrictypes.ClientState), err
+}
+*/
+
+func (lc *Lightclient) CheckProposedHeaderAndUpdateState(header *fabrictypes.Header) (*fabrictypes.ClientState, *fabrictypes.ConsensusState, error) {
+	clientState, consensusState, err := lc.cs.CheckProposedHeaderAndUpdateState(lc.ctx, lc.cdc, lc.store, header)
+	return clientState.(*fabrictypes.ClientState), consensusState.(*fabrictypes.ConsensusState), err
+}
+
+func (lc *Lightclient) VerifyUpgrade(
+	newClient *fabrictypes.ClientState,
+	upgradeHeight clienttypes.Height,
+	proofUpgrade []byte,
+) error {
+	return lc.cs.VerifyUpgrade(lc.ctx, lc.cdc, lc.store, newClient, upgradeHeight, proofUpgrade)
+}
+
+func (lc *Lightclient) ZeroCustomFields() *fabrictypes.ClientState {
+	return lc.cs.ZeroCustomFields().(*fabrictypes.ClientState)
+}
+
+func (lc *Lightclient) VerifyClientState(
+	height clienttypes.Height,
+	prefix *commitmenttypes.MerklePrefix,
+	counterpartyClientIdentifier string,
+	proof []byte,
+	clientState *fabrictypes.ClientState,
+) error {
+	return lc.cs.VerifyClientState(lc.store, lc.cdc, height, prefix, counterpartyClientIdentifier, proof, clientState)
+}
+
+func (lc *Lightclient) VerifyClientConsensusState(
+	height clienttypes.Height,
+	counterpartyClientIdentifier string,
+	consensusHeight clienttypes.Height,
+	prefix *commitmenttypes.MerklePrefix,
+	proof []byte,
+	consensusState *fabrictypes.ConsensusState,
+) error {
+	return lc.cs.VerifyClientConsensusState(lc.store, lc.cdc, height, counterpartyClientIdentifier, consensusHeight, prefix, proof, consensusState)
+}
+
+func (lc *Lightclient) VerifyConnectionState(
+	height clienttypes.Height,
+	prefix *commitmenttypes.MerklePrefix,
+	proof []byte,
+	connectionID string,
+	connectionEnd connectiontypes.ConnectionEnd,
+) error {
+	return lc.cs.VerifyConnectionState(lc.store, lc.cdc, height, prefix, proof, connectionID, connectionEnd)
+}
+
+func (lc *Lightclient) VerifyChannelState(
+	height clienttypes.Height,
+	prefix *commitmenttypes.MerklePrefix,
+	proof []byte,
+	portID,
+	channelID string,
+	channel channeltypes.Channel,
+) error {
+	return lc.cs.VerifyChannelState(lc.store, lc.cdc, height, prefix, proof, portID, channelID, channel)
+}
+
+func (lc *Lightclient) VerifyPacketCommitment(
+	height clienttypes.Height,
+	prefix *commitmenttypes.MerklePrefix,
+	proof []byte,
+	portID,
+	channelID string,
+	sequence uint64,
+	commitmentBytes []byte,
+) error {
+	return lc.cs.VerifyPacketCommitment(lc.store, lc.cdc, height, prefix, proof, portID, channelID, sequence, commitmentBytes)
+}
+
+func (lc *Lightclient) VerifyPacketAcknowledgement(
+	height clienttypes.Height,
+	prefix *commitmenttypes.MerklePrefix,
+	proof []byte,
+	portID,
+	channelID string,
+	sequence uint64,
+	acknowledgement []byte,
+) error {
+	return lc.cs.VerifyPacketAcknowledgement(lc.store, lc.cdc, height, prefix, proof, portID, channelID, sequence, acknowledgement)
+}
+
+func (lc *Lightclient) VerifyPacketReceiptAbsence(
+	height clienttypes.Height,
+	prefix *commitmenttypes.MerklePrefix,
+	proof []byte,
+	portID,
+	channelID string,
+	sequence uint64,
+) error {
+	return lc.cs.VerifyPacketReceiptAbsence(lc.store, lc.cdc, height, prefix, proof, portID, channelID, sequence)
+}
+
+func (lc *Lightclient) VerifyNextSequenceRecv(
+	height clienttypes.Height,
+	prefix *commitmenttypes.MerklePrefix,
+	proof []byte,
+	portID,
+	channelID string,
+	nextSequenceRecv uint64,
+) error {
+	return lc.cs.VerifyNextSequenceRecv(lc.store, lc.cdc, height, prefix, proof, portID, channelID, nextSequenceRecv)
+}
