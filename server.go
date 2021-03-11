923ceadc4dfc4b5bd494597c7a2f6850d358ce79
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2021-03-11 23:12:01 +0900

    stop using nil as gRPC return value because it causes I/O error

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	pb "github.com/datachainlab/fabric-ibc-lightclientd/types"
+	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -35,7 +36,7 @@ func (server) GetFrozenHeight(_ context.Context, req *pb.GetFrozenHeightRequest)
 
 func (server) Validate(_ context.Context, req *pb.ValidateRequest) (*emptypb.Empty, error) {
 	lc := NewLightclient(req.State)
-	return nil, lc.Validate()
+	return &empty.Empty{}, lc.Validate()
 }
 
 func (server) GetProofSpecs(_ context.Context, req *pb.GetProofSpecsRequest) (*pb.GetProofSpecsResponse, error) {
@@ -61,7 +62,7 @@ func (server) CheckProposedHeaderAndUpdateState(ctx context.Context, req *pb.Che
 
 func (server) VerifyUpgrade(_ context.Context, req *pb.VerifyUpgradeRequest) (*emptypb.Empty, error) {
 	lc := NewLightclient(req.State)
-	return nil, lc.VerifyUpgrade(req.NewClient, *req.UpgradeHeight, req.ProofUpgrade)
+	return &empty.Empty{}, lc.VerifyUpgrade(req.NewClient, *req.UpgradeHeight, req.ProofUpgrade)
 }
 
 func (server) ZeroCustomFields(_ context.Context, req *pb.ZeroCustomFieldsRequest) (*pb.ZeroCustomFieldsResponse, error) {
@@ -71,40 +72,40 @@ func (server) ZeroCustomFields(_ context.Context, req *pb.ZeroCustomFieldsReques
 
 func (server) VerifyClientState(_ context.Context, req *pb.VerifyClientStateRequest) (*emptypb.Empty, error) {
 	lc := NewLightclient(req.State)
-	return nil, lc.VerifyClientState(*req.Height, req.Prefix, req.CounterpartyClientIdentifier, req.Proof, req.ClientState)
+	return &empty.Empty{}, lc.VerifyClientState(*req.Height, req.Prefix, req.CounterpartyClientIdentifier, req.Proof, req.ClientState)
 }
 
 func (server) VerifyClientConsensusState(_ context.Context, req *pb.VerifyClientConsensusStateRequest) (*emptypb.Empty, error) {
 	lc := NewLightclient(req.State)
-	return nil, lc.VerifyClientConsensusState(*req.Height, req.CounterpartyClientIdentifier, *req.ConsensusHeight, req.Prefix, req.Proof, req.ConsensusState)
+	return &empty.Empty{}, lc.VerifyClientConsensusState(*req.Height, req.CounterpartyClientIdentifier, *req.ConsensusHeight, req.Prefix, req.Proof, req.ConsensusState)
 }
 
 func (server) VerifyConnectionState(_ context.Context, req *pb.VerifyConnectionStateRequest) (*emptypb.Empty, error) {
 	lc := NewLightclient(req.State)
-	return nil, lc.VerifyConnectionState(*req.Height, req.Prefix, req.Proof, req.ConnectionId, *req.ConnectionEnd)
+	return &emptypb.Empty{}, lc.VerifyConnectionState(*req.Height, req.Prefix, req.Proof, req.ConnectionId, *req.ConnectionEnd)
 }
 
 func (server) VerifyChannelState(_ context.Context, req *pb.VerifyChannelStateRequest) (*emptypb.Empty, error) {
 	lc := NewLightclient(req.State)
-	return nil, lc.VerifyChannelState(*req.Height, req.Prefix, req.Proof, req.PortId, req.ChannelId, *req.Channel)
+	return &empty.Empty{}, lc.VerifyChannelState(*req.Height, req.Prefix, req.Proof, req.PortId, req.ChannelId, *req.Channel)
 }
 
 func (server) VerifyPacketCommitment(_ context.Context, req *pb.VerifyPacketCommitmentRequest) (*emptypb.Empty, error) {
 	lc := NewLightclient(req.State)
-	return nil, lc.VerifyPacketCommitment(*req.Height, req.Prefix, req.Proof, req.PortId, req.ChannelId, req.Sequence, req.CommitmentBytes)
+	return &empty.Empty{}, lc.VerifyPacketCommitment(*req.Height, req.Prefix, req.Proof, req.PortId, req.ChannelId, req.Sequence, req.CommitmentBytes)
 }
 
 func (server) VerifyPacketAcknowledgement(_ context.Context, req *pb.VerifyPacketAcknowledgementRequest) (*emptypb.Empty, error) {
 	lc := NewLightclient(req.State)
-	return nil, lc.VerifyPacketAcknowledgement(*req.Height, req.Prefix, req.Proof, req.PortId, req.ChannelId, req.Sequence, req.Acknowledgement)
+	return &empty.Empty{}, lc.VerifyPacketAcknowledgement(*req.Height, req.Prefix, req.Proof, req.PortId, req.ChannelId, req.Sequence, req.Acknowledgement)
 }
 
 func (server) VerifyPacketReceiptAbsence(_ context.Context, req *pb.VerifyPacketReceiptAbsenceRequest) (*emptypb.Empty, error) {
 	lc := NewLightclient(req.State)
-	return nil, lc.VerifyPacketReceiptAbsence(*req.Height, req.Prefix, req.Proof, req.PortId, req.ChannelId, req.Sequence)
+	return &empty.Empty{}, lc.VerifyPacketReceiptAbsence(*req.Height, req.Prefix, req.Proof, req.PortId, req.ChannelId, req.Sequence)
 }
 
 func (server) VerifyNextSequenceRecv(_ context.Context, req *pb.VerifyNextSequenceRecvRequest) (*emptypb.Empty, error) {
 	lc := NewLightclient(req.State)
-	return nil, lc.VerifyNextSequenceRecv(*req.Height, req.Prefix, req.Proof, req.PortId, req.ChannelId, req.NextSequenceRecv)
+	return &empty.Empty{}, lc.VerifyNextSequenceRecv(*req.Height, req.Prefix, req.Proof, req.PortId, req.ChannelId, req.NextSequenceRecv)
 }
